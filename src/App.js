452210e9f9c7e73dfdef660c4dd892cc07e5f080
Vladislav Kotsev <vladygk8@gmail.com>
2023-01-17 09:45:10 +0200

    Fix: bug

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Document from "./Document";
+
 function App() {
- const [content, setContent] = useState("");
 
- useEffect(()=>{
-     fetch("https://jaspervdj.be/lorem-markdownum/markdown.txt")
-     .then(result=>result.text())
-     .then(text=>setContent(text));
-    
-  },[]);
-
+  const [content, setContent] = useState("");
   
-  return (
-    
-      <Document  title="Terms and Conditions" content={content}/>
+  async function FetchData() {
+    const response = await fetch(
+      "https://jaspervdj.be/lorem-markdownum/markdown.txt"
+    );
+      const data = await response.text();
+     setContent(data);
+  }
+
+  useEffect(() => {
    
-  );
+    FetchData();
+  }, []);
+
+  return <Document title="Terms and Conditions" content={content} />;
 }
 
 export default App;
